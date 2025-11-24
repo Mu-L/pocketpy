0cdcd1701b570df3218a5ced4ed247dec502349c
blueloveTH <blueloveTH@foxmail.com>
2025-11-24 15:09:02 +0800

    some rename
    some fix

--- include/pocketpy/common/threads.h
+++ include/pocketpy/common/threads.h
@@ -19,7 +19,25 @@ typedef thrd_t c11_thrd_t;
 typedef int c11_thrd_retval_t;
 #endif
 
-bool c11_thrd_create(c11_thrd_t* thrd, c11_thrd_retval_t (*func)(void*), void* arg);
-void c11_thrd_yield();
+typedef c11_thrd_retval_t (*c11_thrd_func_t)(void*);
+
+bool c11_thrd__create(c11_thrd_t* thrd, c11_thrd_func_t func, void* arg);
+void c11_thrd__yield();
+
+
+typedef struct c11_thrdpool_worker {
+    c11_thrd_t thread;
+    void* arg;
+    atomic_bool is_idle;
+} c11_thrdpool_worker;
+
+typedef struct c11_thrdpool {
+    int length;
+    c11_thrdpool_worker* workers;
+} c11_thrdpool;
+
+void c11_thrdpool__ctor(c11_thrdpool* pool, int length);
+void c11_thrdpool__dtor(c11_thrdpool* pool);
+bool c11_thrdpool__create(c11_thrdpool* pool, c11_thrd_func_t func, void* arg);
 
 #endif
--- src/common/threads.c
+++ src/common/threads.c
@@ -1,25 +1,66 @@
 #include "pocketpy/common/threads.h"
+#include "pocketpy/common/utils.h"
 
 #if PK_ENABLE_THREADS
 
 #if PK_USE_PTHREADS
 
-bool c11_thrd_create(c11_thrd_t* thrd, c11_thrd_retval_t (*func)(void*), void* arg) {
+bool c11_thrd__create(c11_thrd_t* thrd, c11_thrd_func_t func, void* arg) {
     int res = pthread_create(thrd, NULL, func, arg);
     return res == 0;
 }
 
-void c11_thrd_yield() { sched_yield(); }
+void c11_thrd__yield() { sched_yield(); }
 
 #else
 
-bool c11_thrd_create(c11_thrd_t* thrd, c11_thrd_retval_t (*func)(void*), void* arg) {
+bool c11_thrd__create(c11_thrd_t* thrd, c11_thrd_func_t func, void* arg) {
     int res = thrd_create(thrd, func, arg);
     return res == thrd_success;
 }
 
-void c11_thrd_yield() { thrd_yield(); }
+void c11_thrd__yield() { thrd_yield(); }
 
 #endif
+
+static c11_thrd_retval_t _thrdpool_worker(void* arg) {
+    while (true) {
+        c11_thrd__yield();
+    }
+}
+
+void c11_thrdpool__ctor(c11_thrdpool* pool, int length) {
+    pool->length = length;
+    pool->workers = PK_MALLOC(sizeof(c11_thrdpool_worker) * length);
+    for (int i = 0; i < length; i++) {
+        bool ok = c11_thrd__create(&pool->workers[i].thread, _thrdpool_worker, NULL);
+        c11__rtassert(ok);
+        pool->workers[i].arg = NULL;
+        atomic_store(&pool->workers[i].is_idle, true);
+    }
+}
+
+void c11_thrdpool__dtor(c11_thrdpool* pool) {
+    for (int i = 0; i < pool->length; i++) {
+        // Note: No proper thread joining or termination is done here.
+        // This is a placeholder for demonstration purposes.
+    }
+    PK_FREE(pool->workers);
+}
+
+bool c11_thrdpool__create(c11_thrdpool* pool, c11_thrd_func_t func, void* arg) {
+    // find the 1st idle worker
+    for (int i = 0; i < pool->length; i++) {
+        bool expected = true;
+        if (atomic_compare_exchange_strong(&pool->workers[i].is_idle, &expected, false)) {
+            // Found an idle worker, assign the task
+            // Note: In a real implementation, you would need a way to pass the function and argument to the worker thread.
+            // This is a placeholder for demonstration purposes.
+            return true;
+        }
+    }
+    return false;  // No idle worker found
+}
+
 
 #endif  // PK_ENABLE_THREADS
